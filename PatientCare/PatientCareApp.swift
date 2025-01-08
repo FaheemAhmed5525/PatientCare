2c365aed1733e03053db9f5d7e7406fd87be0c2d
Faheem Ahmed <faheemahmed5525@gmail.com>
2025-01-08 09:51:43 +0500

    Initial Commit

---
+++ PatientCare/PatientCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatientCareApp.swift
+//  PatientCare
+//
+//  Created by Faheem Ahmed on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct PatientCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
